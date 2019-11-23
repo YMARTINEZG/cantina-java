371d09bbd91cb731a384f76dbdeed5a3715f35f0
Yvan Martinez <yvangamboa@gmail.com>
2019-11-23 16:29:14 -0500

    initial setup for spring boot project

---
+++ src/main/java/com/exercise/cantinajava/CantinaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.cantinajava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CantinaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CantinaJavaApplication.class, args);
+	}
+
+}
